7487b5e0c4a8af7eec0c5212710bde3d5e341783
Валерия <@DESKTOP-89Q1D6N>
2022-05-09 14:57:50 +0300

    main page with table view and entries

--- easyCRM/easyCRM/MainPage.xaml.cs
+++ easyCRM/easyCRM/MainPage.xaml.cs
@@ -10,9 +10,53 @@ namespace easyCRM
 {
     public partial class MainPage : ContentPage
     {
+        TableView tableView;
+        EntryCell name, phone, date, amount;
+        ImageCell img_order;
+        TableSection fotosection, nuppud;
+        Button sms_btn;
+        Button call_btn;
+        Button email_btn;
+        StackLayout st;
         public MainPage()
         {
-            InitializeComponent();
+            call_btn = new Button { Text = "Helista" }; call_btn.Clicked += Call_btn_Clicked;
+            sms_btn = new Button { Text = "Saada sms" }; sms_btn.Clicked += Sms_btn_Clicked;
+            email_btn = new Button { Text = "Saada email" }; email_btn.Clicked += Email_btn_Clicked;
+            phone = new EntryCell
+            {
+                Label= "Telefon:",
+                Placeholder = "Sissesta number",
+                Keyboard = Keyboard.Telephone
+            };
+            date = new EntryCell
+            {
+                Label = "Arve kuupäev:",
+                Placeholder = "Sissesta kuupaev dd/mm/yyyy",
+                Keyboard = Keyboard.Text
+            };
+            amount = new EntryCell
+            {
+                Label = "Kokku",
+                Placeholder = "Kokku",
+                Keyboard = Keyboard.Numeric
+            };
+            st = new
+        }
+
+        private void Email_btn_Clicked(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Sms_btn_Clicked(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Call_btn_Clicked(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
         }
     }
 }
