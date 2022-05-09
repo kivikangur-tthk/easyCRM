f0ee9a94616b7664f5e61856eae543a1fbb085ef
Валерия <@DESKTOP-89Q1D6N>
2022-05-09 15:21:50 +0300

    #182048850 Create new main age

--- easyCRM/easyCRM/MainPage.xaml.cs
+++ easyCRM/easyCRM/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Plugin.Messaging;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -10,9 +11,113 @@ namespace easyCRM
 {
     public partial class MainPage : ContentPage
     {
+        TableView tableView;
+        EntryCell name, phone, date, amount, tex;
+        ImageCell img_order;
+        TableSection fotosection, nuppud;
+        Button sms_btn;
+        Button call_btn;
+        Button sub;
+        StackLayout st;
         public MainPage()
         {
-            InitializeComponent();
+            call_btn = new Button { Text = "Helista" }; call_btn.Clicked += Call_btn_Clicked;
+            sms_btn = new Button { Text = "Saada sms" }; sms_btn.Clicked += Sms_btn_Clicked;
+            sub = new Button { Text = "Lisa arve" }; sub.Clicked += Sub_Clicked;
+
+            phone = new EntryCell
+            {
+                Label = "Telefon:",
+                Placeholder = "Sissesta number",
+                Keyboard = Keyboard.Telephone
+            };
+            date = new EntryCell
+            {
+                Label = "Arve kuupäev:",
+                Placeholder = "Sissesta kuupaev dd/mm/yyyy",
+                Keyboard = Keyboard.Text
+            };
+            amount = new EntryCell
+            {
+                Label = "Kokku",
+                Placeholder = "Kokku",
+                Keyboard = Keyboard.Numeric
+            };
+            tex = new EntryCell
+            {
+                Label = "Sms tekst",
+                Placeholder = "Kirjuta teks sms-le",
+                Keyboard = Keyboard.Default
+            };
+            st = new StackLayout
+            {
+                Children = { call_btn, sms_btn, sub },
+                Orientation = StackOrientation.Horizontal
+            };
+            nuppud = new TableSection
+            {
+                new ViewCell() { View=st},
+            };
+            img_order = new ImageCell
+            {
+                ImageSource = ImageSource.FromFile("Fox.jpg"),
+                Text = "Foto nimetus",
+                Detail = "Foto kirjeldus"
+            };
+            fotosection = new TableSection();
+
+            tableView = new TableView
+            {
+                Intent = TableIntent.Form,
+                Root = new TableRoot("Andmete sissestamine")
+                {
+                    new TableSection("Põhiandmed:")
+                    {
+                        new EntryCell
+                        {
+                            Label="Nimi:",
+                            Placeholder="Sissesta oma sõbra nimi",
+                            Keyboard=Keyboard.Default
+                        }
+                    },
+                    new TableSection("Kontaktandmed:")
+                    {
+                        phone,
+                        date,
+                        amount,
+                        tex
+                    },
+                    fotosection,
+                    nuppud
+                }
+
+            };
+            Content = tableView;
+
+
+        }
+
+        private void Sub_Clicked(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Sms_btn_Clicked(object sender, EventArgs e)
+        {
+            var sms = CrossMessaging.Current.SmsMessenger;
+            if (sms.CanSendSms)
+            {
+                sms.SendSms(phone.Text, tex.Text);
+            }
+        }
+
+        private void Call_btn_Clicked(object sender, EventArgs e)
+        {
+            var call = CrossMessaging.Current.PhoneDialer;
+            if (call.CanMakePhoneCall)
+            {
+                call.MakePhoneCall(phone.Text);
+            }
         }
     }
 }
